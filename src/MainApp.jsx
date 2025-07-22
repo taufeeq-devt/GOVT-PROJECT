cfe03bb572f94564bbe6765488c7b40cdede5543
noblepaul995 <noblepaul995@gmail.com>
2025-07-22 13:30:23 +0530

    first commit

---
+++ src/MainApp.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import DashboardHome from './components/projectManager/pages/DashboardHome';
+
+const MainApp = () => {
+  
+
+  return (
+    <>
+    <DashboardHome/>
+    </>
+  );
+};
+
+export default MainApp;
